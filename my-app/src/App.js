ea3318a4042cc937a1d438a9fef78e5197fd52c8
Ken Tung <kentung@hawaii.edu>
2021-02-21 23:32:07 -1000

    new interface

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar';
+import Login from "./components/login.js";
+import SignUp from "./components/signup.js";
+import Welcome from "./components/welcome.js";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/"}>Manoa Board</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path='/' component={Welcome} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div></Router>
   );
 }
 
