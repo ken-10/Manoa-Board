97c7a59246417a14709356b850b52e2789ba1323
QiQing-Luo <42788279+QiQing-Luo@users.noreply.github.com>
2021-03-28 23:27:59 -1000

    Update Welcome.js

--- my-app/src/components/welcome.js
+++ my-app/src/components/welcome.js
@@ -8,6 +8,127 @@ export default class Welcome extends Component {
                 <Row>
                     <Col>
                         <h1>Welcome to Manoa Board.</h1>
+                        <div class="ui borderless topmenu menu">
+                            <div class="ui container">
+                                <a class="fitted right item"><i class="map marker alternate icon"></i></a>
+                                <div class="ui dropdown fitted item">
+                                    <div class="text">Near By</div>
+                                </div>
+                                <div class="ui dropdown item">
+                                    <div class="text">|</div>
+                                </div>
+                                <div class="ui dropdown item">
+                                    <div class="text">Sign In</div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="ui borderless middlemenu menu">
+                            <img id="logo" class="ui left image"
+                                 src="https://www.outreach.hawaii.edu/wp-content/uploads/2019/10/43612925_10156021290584001_7149835124257849344_n.jpg">
+
+                            <div class="ui centered grid container">
+                                <div class="ui dropdown item">
+                                    <div class="text">COMMUNITY</div>
+                                    <i class="dropdown icon"></i>
+                                    <div class="menu">
+                                        <div class="item">Manoa</div>
+                                        <div class="item">Find Your Place</div>
+                                    </div>
+                                </div>
+
+                                <div class="ui dropdown item">
+                                    <div class="text">SOCIAL IMPACT</div>
+                                    <i class="dropdown icon"></i>
+                                    <div class="menu">
+                                        <div class="item">Community</div>
+                                        <div class="item">Ethical Sourcing</div>
+                                    </div>
+                                </div>
+
+                                <div class="ui dropdown item">
+                                    <div class="text">CAMPUS LIFE</div>
+                                    <i class="dropdown icon"></i>
+                                    <div class="menu">
+                                        <div class="item">Arts and Culture</div>
+                                        <div class="item">Food and Housing</div>
+                                    </div>
+                                </div>
+
+                                <div class="ui dropdown item">
+                                    <div class="text">VIEW</div>
+                                    <i class="dropdown icon"></i>
+                                    <div class="menu">
+                                        <div class="item">Your Posts</div>
+                                        <div class="item">Others Posts</div>
+                                    </div>
+                                </div>
+
+                                <div class="ui dropdown item">
+                                    <div class="text">SEARCH</div>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+
+                        <div class="mainPage">
+                            <div class="ui grid">
+                                <div class="eight wide column">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="footer-background">
+                            <div class="ui three column grid container">
+                                <div class="column">
+                                    ABOUT US
+                                    <hr/>
+                                    <div class="ui list">
+                                        <div class="item">Our Board</div>
+                                        <div class="item">Background</div>
+                                    </div>
+                                </div>
+                                <div class="column">
+                                    CAREER CENTER
+                                    <hr/>
+                                    <div class="ui list">
+                                        <div class="item">Join Us?</div>
+                                        <div class="item">College Plan</div>
+                                    </div>
+                                </div>
+                                <div class="column">
+                                    QUICK LINKS
+                                    <hr/>
+                                    <div class="ui list">
+                                        <div class="item">My Account</div>
+                                        <div class="item">Edit Posts</div>
+                                        <div class="item">Make a Post</div>
+                                        <div class="item"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="ui borderless downmenu menu">
+                            <div class="ui container">
+                                <a class="fitted item"><i class="us flag"></i></a>
+                                <div class="ui dropdown fitted item">
+                                    <div class="text">Change Region »</div>
+                                </div>
+                                <div class="ui dropdown item">
+                                    <div class="text">|</div>
+                                </div>
+                                <div class="ui dropdown item">
+                                    <div class="text">English</div>
+                                </div>
+
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
