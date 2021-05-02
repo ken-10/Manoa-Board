1d9a31aeb4a6ad0a1ac69857e316afc2592687fe
Jiaying Feng <jiayingf@hawaii.edu>
2021-05-01 15:56:42 -1000

    add select file in make post page

--- my-app/src/components/post/makePost.js
+++ my-app/src/components/post/makePost.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css';
+import {Header, Container, Button} from 'semantic-ui-react';
 
 export default class makePost extends Component {
   render() {
@@ -21,6 +24,61 @@ export default class makePost extends Component {
         )
       }
     }
+//no file selected
+    state = {
+      selectedFile: null
+    };
+
+    //select file
+    onFileChange = event => {
+      this.setState({ selectedFile: event.target.files[0] });
+    };
+
+    // On file upload (click the upload button)
+    onFileUpload = () => {
+
+      // Create an object of formData
+      const formData = new FormData();
+
+      // Update the formData object
+      formData.append(
+          "myFile",
+          this.state.selectedFile,
+          this.state.selectedFile.name
+      );
+
+      // Details of the uploaded file
+      console.log(this.state.selectedFile);
+
+      // Request made to the backend api
+      // Send formData object
+      axios.post("api/uploadfile", formData);
+    };
+
+    // File content to be displayed after file upload is complete
+    fileData = () => {
+      if (this.state.selectedFile) {
+        return (
+            <div>
+              <h2>File Details:</h2>
+
+              <p>File Name: {this.state.selectedFile.name}</p>
+              <p>File Type: {this.state.selectedFile.type}</p>
+              <p>
+                Last Modified:{" "}
+                {this.state.selectedFile.lastModifiedDate.toDateString()}
+              </p>
+            </div>
+        );
+      } else {
+        return (
+            <div>
+              <br />
+              <h4>Choose before Pressing the Upload button</h4>
+            </div>
+        );
+      }
+    };
 
     class MiddleMenu extends React.Component {
       render() {
@@ -35,7 +93,15 @@ export default class makePost extends Component {
 
                 <div className="form-group" style={{ paddingBottom: "15px" }}>
                   <label>Select pictures</label>
-                  <input type="image" className="form-control" placeholder="Select images"/>
+                  <p>
+                    Click on the "Browse" button to browse for pictures:
+                  </p>
+                  <input type="file" style={{ color: 'gray', border: '1px solid gainsboro', borderRadius: '15px', padding: '15px' }}       onChange={this.onFileChange} />
+                  <br/><br/>
+                  <Button onClick={this.onFileUpload}>
+                    Upload
+                  </Button>
+                  {this.fileData()}
                 </div>
               </Grid>
             </Menu>
