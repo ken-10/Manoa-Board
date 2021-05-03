48378bcab510a6b47170b61713566d6c6c86e7af
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2021-05-02 21:50:03 -1000

    Added View Post modal

--- my-app/src/components/gallery.js
+++ my-app/src/components/gallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import {database} from "../firebase";
 import './gallery.css';
@@ -9,8 +9,6 @@ export class GalleryModal extends React.Component {
   constructor() {
     super();
     this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.state = { show: this.props.show };
   }
   componentDidMount() {
     document.body.addEventListener('keydown', this.handleKeyDown);
@@ -26,49 +24,47 @@ export class GalleryModal extends React.Component {
     if (e.keyCode === 39 && this.props.hasNext)
       this.props.findNext();
   }
-  handleClose(e) {
-    this.setState({show: false});
-    this.props.closeModal();
-  }
 
   render () {
-    const { closeModal, hasNext, hasPrev, findNext, findPrev, src } = this.props;
-    if (!src) {
-      console.log('whut')
+    const { closeModal, hasNext, hasPrev, findNext, findPrev, post } = this.props;
+    if (!post) {
+      console.log('modal is closed')
       return null;
     }
     return (
-      <Modal show={this.state.show} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <div>
+        <div className="modal-overlay" onClick={closeModal}></div>
+        <div isOpen={!!post} className="post-modal">
+          <div className='post-modal-body'>
+            <a href="#" className='modal-close' onClick={closeModal} onKeyDown={this.handleKeyDown}>&times;</a>
+            {hasPrev && <a href="#" className='modal-prev' onClick={findPrev} onKeyDown={this.handleKeyDown}>&lsaquo;</a>}
+            {hasNext && <a href="#" className='modal-next' onClick={findNext} onKeyDown={this.handleKeyDown}>&rsaquo;</a>}
+            <img src={post.image} />
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-const imgUrls = ['https://source.unsplash.com/PC_lbSSxCZE/800x600','https://source.unsplash.com/lVmR1YaBGG4/800x600','https://source.unsplash.com/5KvPQc1Uklk/800x600','https://source.unsplash.com/GtYFwFrFbMA/800x600','https://source.unsplash.com/Igct8iZucFI/800x600','https://source.unsplash.com/M01DfkOqz7I/800x600','https://source.unsplash.com/MoI_cHNcSK8/800x600','https://source.unsplash.com/M0WbGFRTXqU/800x600','https://source.unsplash.com/s48nn4NtlZ4/800x600','https://source.unsplash.com/E4944K_4SvI/800x600','https://source.unsplash.com/F5Dxy9i8bxc/800x600','https://source.unsplash.com/iPum7Ket2jo/800x600'
-];
+
 const Gallery = (props) => {
   const [currentIndex, setCurrentIndex] = useState(null);
+
+  useEffect(() => {
+    console.log(currentIndex)
+  }, [currentIndex])
+
   const renderImageContent = (post, index) => {
     return (
       <div  onClick={(e) => openModal(e, index)}>
-        <img src={post.image} key={0} alt=""/>
+        <img src={post.image} key={post.postKey} alt=""/>
       </div>
     ) 
   }
   const openModal = (e, index) => {
     setCurrentIndex(index);
+    // console.log(currentIndex)
   }
   const closeModal = (e) => {
     if (e !== undefined) {
@@ -80,17 +76,13 @@ const Gallery = (props) => {
     if (e !== undefined) {
       e.preventDefault();
     }
-    setCurrentIndex(prevState => ({
-      currentIndex: prevState.currentIndex - 1
-    }));
+    setCurrentIndex(currentIndex - 1);
   }
   const findNext = (e) => {
     if (e !== undefined) {
       e.preventDefault();
     }
-    setCurrentIndex(prevState => ({
-      currentIndex: prevState.currentIndex + 1
-    }));
+    setCurrentIndex(currentIndex + 1);
   }
 
   return (
@@ -98,14 +90,14 @@ const Gallery = (props) => {
       <div className="gallery-grid">
         {props.data.map(renderImageContent)}
       </div>
-      {/* <GalleryModal 
+      <GalleryModal 
         closeModal={closeModal} 
         findPrev={findPrev} 
         findNext={findNext} 
         hasPrev={currentIndex > 0} 
-        hasNext={currentIndex + 1 < imgUrls.length} 
-        src={props.data[currentIndex]} 
-      /> */}
+        hasNext={currentIndex + 1 < props.data.length} 
+        post={props.data[currentIndex]} 
+      />
     </div>
   );
 }
--- my-app/src/components/welcome.js
+++ my-app/src/components/welcome.js
@@ -4,13 +4,16 @@ import {Container, Row, Col} from "react-bootstrap";
 export default class Welcome extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>Welcome to Manoa Board.</h1>
-                    </Col>
-                </Row>
-            </Container>
+            <div className='inner'>
+                <h1>Welcome to Manoa Board.</h1>
+            </div>
+            // <Container>
+            //     <Row>
+            //         <Col>
+            //             <h1>Welcome to Manoa Board.</h1>
+            //         </Col>
+            //     </Row>
+            // </Container>
         );
     }
 }
