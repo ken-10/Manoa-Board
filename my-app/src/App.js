3cab12e8a21f0f7f0602846901a543cd0f59a588
QiQing-Luo <42788279+QiQing-Luo@users.noreply.github.com>
2021-05-02 16:02:10 -1000

    Dashboard page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 import Login from "./components/login.js";
 import SignUp from "./components/signup.js";
 import Welcome from "./components/welcome.js";
 import makePost from "./components/makePost.js";
+import Dashboard from "./components/Dashboard.js";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
@@ -27,6 +29,9 @@ function App() {
                 <li className="nav-item">
                   <Link className="nav-link" to={"/make-post"}>Make Post</Link>
                 </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/dashboard"}>Dashboard</Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -39,6 +44,7 @@ function App() {
               <Route path="/sign-in" component={Login} />
               <Route path="/sign-up" component={SignUp} />
               <Route path="/make-post" component={makePost}/>
+              <Route path="/dashboard" component={Dashboard}/>
             </Switch>
           </div>
         </div>
---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Container, Input, Header, Grid, Card, Label, Icon } from 'semantic-ui-react';
+
+const Dashboard = () => {
+
+    const rows = [];
+    let row = [];
+    for (let i = 1; i <= 7; i++) {
+        for (let j = 1; j <= 3; j++) {
+            row.push(
+                <Grid.Column>
+                    <Card>
+                        <Label corner='right' size='small'><Icon name='eye' /></Label>
+                        <Card.Content textAlign='center'>
+                            <Card.Header>Account {3 * (i - 1 ) + j} Name</Card.Header>
+                        </Card.Content>
+                        <Card.Content extra textAlign='right'>
+                            <Card.Description>
+                                <Header as='h4' textAlign='left'>Post</Header>
+                                UH Manoa
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra textAlign='right'>
+                            <Card.Description>
+                                <Header as='h4' textAlign='left'>Tag People</Header>
+                                Jonny Dang
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra textAlign='right'>
+                            <Card.Description>
+                                <Header as='h4' textAlign='left'>Add Location</Header>
+                                Honolulu, Hawaii
+                            </Card.Description>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            );
+        }
+        rows.push(<Grid.Row>{row}</Grid.Row>);
+        row = [];
+    }
+
+    return (
+        <Container>
+            <Header textAlign='center' as='h2'>UH Manoa Dashboard</Header>
+            <Grid relaxed columns={1} style={{ overflowY: 'auto', height: '60vh' }}>
+                {rows.map(row => { return row; })}
+            </Grid>
+        </Container>
+    );
+}
+
+export default Dashboard;
